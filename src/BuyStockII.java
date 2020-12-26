f11d758d1d5f5fda3f6eaee8bf8de6ac4cc1a044
Gu Guoli <guguolii@sina.com>
2020-12-26 20:19:45 +0800

    Rotate an array

--- src/BuyStockII.java
+++ src/BuyStockII.java
@@ -2,6 +2,8 @@
 /**
  * 买卖股票的最佳时机 II
  */
+
+ // 贪心法
 public class BuyStockII {
     public int maxProfit(int[] prices)
     {
@@ -17,5 +19,21 @@ public class BuyStockII {
         }
 
         return profit;
+    }
+
+    // 动态规划
+    public int maxProfit2 (int[] prices)
+    {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i=1; i < n; i++)
+        {
+            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] - prices[i]);
+        }
+
+        return dp[n-1][0];
     }
 }
--- src/RotateTheArray.java
+++ src/RotateTheArray.java
@@ -4,9 +4,81 @@
  */
 public class RotateTheArray {
 
+    // 暴力
     public void rotate(int[] nums, int k)
     {
-        ;
+        int temp, previous;
+        for (int i=0; i<k; i++)
+        {
+            previous = nums[nums.length-1];
+            for (int j=0; j< nums.length; j++)
+            {
+                temp = nums[j];
+                nums[j] = previous;
+                previous = temp;
+            }
+        }
+    }
+
+    // 使用额外的数组
+    // 空间复杂度 O(n)
+    public void rotate2(int[] nums, int k)
+    {
+        int[] a = new int[nums.length];
+        for (int i=0; i<nums.length; i++) 
+        {
+            a[(i+k)%nums.length] = nums[i];
+        }
+
+        for (int i=0; i<nums.length; i++) 
+        {
+            nums[i] = a[i];
+        }
+
+    }
+
+    // 使用环状替换 time O(n) space O(1)
+    public void rotate3(int[] nums, int k)
+    {
+        k = k % nums.length;
+        int count = 0;
+        for(int start = 0; start < nums.length; start++)
+        {
+            int current = start;
+            int prev = nums[start];
+            do
+            {
+                int next = (current + k) % nums.length;
+                int temp = nums[next];
+                nums[next] = prev;
+                prev = temp;
+
+                current = next;
+                count++;
+
+            } while (start != current);
+        }
+    }
+
+    // 使用反转
+    public void reverse(int[] nums, int start, int end)
+    {
+        while(start < end)
+        {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public void rotate4(int[] nums, int k)
+    {
+        k %= nums.length;
+        reverse(nums, 0, nums.length-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, nums.length-1);
     }
     
 }
